f431bb7b0ac734b2ec391f8c8d71bbdf128b1d1f
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-20 17:31:40 -0700

    add an executable main.rb file to bin directory

---
+++ bin/main.rb
@@ -0,0 +1,11 @@
+# !/usr/bin/env ruby
+require 'csv'
+require_relative '../lib/tastee.rb'
+
+tastee = Tastee.new
+
+tastee.scrape
+
+CSV.open('tastee.csv', 'w') do |csv|
+	csv << tastee.tastee_locations
+end
