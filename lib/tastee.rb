a53dfae294c47046c98eb4c3839972a7291d7478
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-20 17:32:58 -0700

    add Tastee Class with private and public methods

---
+++ lib/tastee.rb
@@ -0,0 +1,25 @@
+require 'nokogiri'
+require 'httparty'
+
+class Tastee
+    attr_reader :tastee_locations
+
+  def initialize
+    @url = 'https://www.tfc.com.ng/locations'
+    @tastee_locations = []
+     end
+
+  def scrape
+    parsed_page = parse_url(@url)
+    parsed_page.css('div.branch_location').css('h4 > span').map do |address| 
+        location_address = address.text
+        @tastee_locations.push(location_address)
+    end
+    end
+
+  private
+  def parse_url(url)
+    unparsed_page = HTTParty.get(url)
+    Nokogiri::HTML(unparsed_page)
+  end
+end
